60377fc4346550b06042e36b0b5df2b0d934c17e
Olivia <olivia.rotari2602@gmail.com>
2025-03-15 20:59:46 +0200

    citirea unui nr intreg

--- Rotari_Olivia_ActivitateSD2025/Seminar01.c
+++ Rotari_Olivia_ActivitateSD2025/Seminar01.c
@@ -3,7 +3,10 @@
 
 int main()
 {
-	printf("Salutare");
+	printf("Salutare!\n Introduceti un numar intreg: " );
+	int variabila = 0;
+	scanf_s("%d", &variabila);
+	printf("Ai introdus : %d", variabila);
 	return 0;
 
 }
